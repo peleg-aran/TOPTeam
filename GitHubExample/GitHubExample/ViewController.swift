28e8da32d83ce970041ebdd014cb1b7145eed3d0
peleg.aran <peleg.aran@gmail.com>
2014-12-25 12:19:00 +0200

    peleg new project
    added new files

---
+++ GitHubExample/GitHubExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubExample
+//
+//  Created by Peleg Aran on 12/25/14.
+//  Copyright (c) 2014 Peleg Aran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        println("peleg")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
